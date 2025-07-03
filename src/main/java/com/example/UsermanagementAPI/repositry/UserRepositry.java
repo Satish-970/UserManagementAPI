5e3f90087b594f867494a4f18f12b57deeeb6555
Satish-970 <satishpakalapati65@gmail.com>
2025-07-03 13:15:45 +0530

    Mock data

--- src/main/java/com/example/UsermanagementAPI/repositry/UserRepositry.java
+++ src/main/java/com/example/UsermanagementAPI/repositry/UserRepositry.java
@@ -3,10 +3,11 @@ package com.example.UsermanagementAPI.repositry;
 import com.example.UsermanagementAPI.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface UserRepositry  extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     List<User> findByNameContainingIgnoreCase(String name);
